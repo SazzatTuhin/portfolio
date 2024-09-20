caf34d066b21905041de365e018e58a0f1f71bfc
sazzat-tuhin <44683687+SazzatTuhin@users.noreply.github.com>
2024-09-20 16:51:29 +0600

    resume link update

--- constants/resume.ts
+++ constants/resume.ts
@@ -1,2 +1,2 @@
 export const RESUME_DRIVE_LINK =
-  "https://drive.google.com/uc?export=download&id=1zAbqXET47qDNgUEA8xmVSXrvfa1clPmV";
+  "https://drive.google.com/uc?export=download&id=1bs3HEyWaMoZ-jQRoOgvrzqTo3w4HN-QJ";
