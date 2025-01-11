a76871067de2654285ba205b27eac4622ca169e1
sazzat-tuhin <44683687+SazzatTuhin@users.noreply.github.com>
2025-01-11 17:22:03 +0600

    Add a new blog

--- data/blogs.ts
+++ data/blogs.ts
@@ -16,4 +16,13 @@ export const blogs: {
     body: "In web development, tools that help streamline the process and increase productivity are always valuable. Tailwind CSS has received a lot of attention lately, giving developers more control over their designs without the usual hassle. Let's explore why Tailwind CSS is a top choice for modern web projects.",
     url: "https://www.linkedin.com/pulse/why-tailwind-css-must-have-web-developers-m-m-sazzat-hossain-ojqwc/?trackingId=ofr5nwgjQ4%2BBrlkVRGXmKA%3D%3D",
   },
+  {
+    id: 2,
+    title: "Next.js vs. React- Which One is Best for You in 2025?",
+    cover: "/React.png",
+    author: "Sazzat Hossain",
+    publishedAt: "Jan 11, 2025",
+    body: "As developers, we often get caught up in the dilemma of choosing the right technology for the project. Two of the most popular options in the JavaScript ecosystem are React and Next.js. Although both of these tools are excellent for building user interfaces, you should always use the correct one for the purpose you intend it for, as they each have their strengths. This article breaks down the key differences between them in order to help you make an informed decision on the one that may be the better choice for your next project.",
+    url: "https://www.linkedin.com/pulse/nextjs-vs-react-which-one-best-you-2025-m-m-sazzat-hossain-qbbdc/",
+  },
 ];
